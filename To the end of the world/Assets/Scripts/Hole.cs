482d2de46df5214ed89ff536f355ce500572dd02
herbyvor <50072047+jeanfrancoisemil@users.noreply.github.com>
2023-12-12 13:54:47 +0100

    first push

---
+++ To the end of the world/Assets/Scripts/Hole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+
+namespace DefaultNamespace
+{
+    public class Hole : MonoBehaviour
+    {
+        //le gameobject ou on veut que le player ressorte en remontant
+        public GameObject sortie;
+        //l'id de ce trou
+        public int number;
+        //la scene et l'id du tunel vers lequel il mène
+        public String nextTunelScene;
+        public int nextTunelNumber;
+
+        private GameManager gameManager;
+
+        private void Start()
+        {
+            gameManager = GameManager.Instance;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                gameManager.StartCoroutine(gameManager.goDown(this));
+                
+            }
+        }
+    }
+}
---
+++ To the end of the world/Assets/Scripts/Tunel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class Tunel : MonoBehaviour
+    {
+        public int numero;
+    }
+}
